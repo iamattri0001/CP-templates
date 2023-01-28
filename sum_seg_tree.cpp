cc80a519a3c3762235e48564f76c908b83fc823e
iamattri0001 <105377205+iamattri0001@users.noreply.github.com>
2023-01-29 00:08:08 +0530

    Create sum_seg_tree.cpp

---
+++ sum_seg_tree.cpp
@@ -0,0 +1,93 @@
+//lazy propagation 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define leftChild(i) (2 * i + 1)
+#define rightChild(i) (2 * i + 2)
+#define midPt(low, high) (low + (high - low) / 2)
+#define INF 1e9
+#define MIN_INF -1e9
+
+int lazy[int(1e5)] = {0}; // lazy is declared global, make it zero after every test case
+
+void buildTree(int arr[], int start, int end, int *tree, int i = 0)
+{
+    if (start == end)
+    {
+        tree[i] = arr[start];
+        return;
+    }
+
+    int mid = midPt(start, end);
+    buildTree(arr, start, mid, tree, leftChild(i));
+    buildTree(arr, mid + 1, end, tree, rightChild(i));
+
+    tree[i] = tree[leftChild(i)] + tree[rightChild(i)];
+}
+
+int queryLazy(int q_start, int q_end, int *tree, int low, int high, int i = 0)
+{
+    if (lazy[i] != 0)
+    {
+        tree[i] += lazy[i];
+        lazy[leftChild(i)] += lazy[i];
+        lazy[rightChild(i)] += lazy[i];
+        lazy[i] = 0;
+    }
+
+    if (q_start > high || q_end < low)
+        return 0;
+
+    if (low >= q_start && high <= q_end)
+        return tree[i];
+    int mid = midPt(low, high);
+    int left = queryLazy(q_start, q_end, tree, low, mid, leftChild(i));
+    int right = queryLazy(q_start, q_end, tree, mid + 1, high, rightChild(i));
+
+    return left + right;
+}
+
+void updateLazy(int q_start, int q_end, int change, int *tree, int low, int high, int i = 0)
+{
+    // for single point update, pass (q_start = q_end = the index of point to be updated)
+    if (lazy[i] != 0)
+    {
+        tree[i] += lazy[i] * (high - low + 1);
+        lazy[leftChild(i)] += lazy[i];
+        lazy[rightChild(i)] += lazy[i];
+        lazy[i] = 0;
+    }
+
+    if (q_end < low || q_start > high)
+        return;
+
+    if (low >= q_start && high <= q_end)
+    {
+        tree[i] += (change * (high - low + 1));
+        lazy[leftChild(i)] += change;
+        lazy[rightChild(i)] += change;
+        return;
+    }
+
+    int mid = midPt(low, high);
+    updateLazy(q_start, q_end, change, tree, low, mid, leftChild(i));
+    updateLazy(q_start, q_end, change, tree, mid + 1, high, rightChild(i));
+
+    tree[i] = tree[leftChild(i)] + tree[rightChild(i)];
+    return;
+}
+
+int main()
+{
+    int arr[] = {2, 3, 5, -9, 18, 4, 27, 8};
+    int n = sizeof(arr) / sizeof(int);
+    int t_size = 4 * n;
+    int *tree = new int[t_size];
+    buildTree(arr, 0, n - 1, tree);
+
+    updateLazy(4, 7, 10, tree, 0, n - 1); // range update
+    updateLazy(4, 4, 10, tree, 0, n - 1); // point update
+
+    cout << queryLazy(0, n - 1, tree, 0, n - 1) << endl;
+}

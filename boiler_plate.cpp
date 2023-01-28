c9bd01dccd2c91f47023de3e3a2b3ee0b0b30245
iamattri0001 <105377205+iamattri0001@users.noreply.github.com>
2023-01-28 23:11:08 +0530

    Create boiler_plate.cpp

---
+++ boiler_plate.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define vi vector<int>
+#define vll vector<long long int>
+#define pb push_back
+#define loop(n) for (int i = 0; i < n; i++)
+#define loopRange(a, b) for (int i = a; i < b; i++)
+#define INF 1e9
+#define MIN_INF -1e9
+#define sortAll(v) sort(v.begin(), v.end())
+#define newL cout << "\n"
+#define vec_in(v, n)            \
+    for (int i = 0; i < n; i++) \
+    cin >> v[i]
+
+#define vec_out(v, n)           \
+    for (int i = 0; i < n; i++) \
+    cout << v[i] << " "
+
+#define umap unordered_map
+#define uset unordered_set
+
+
+int32_t main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        /*******/
+
+        newL;
+    }
+    return 0;
+}

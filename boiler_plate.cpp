7766f8923f90ac4bb2bf68a86e36a75ce7e99f91
iamattri0001 <105377205+iamattri0001@users.noreply.github.com>
2023-02-10 15:34:21 +0530

    Update boiler_plate.cpp

--- boiler_plate.cpp
+++ boiler_plate.cpp
@@ -25,6 +25,16 @@ using namespace std;
 
 int32_t main()
 {
+     
+#ifndef ONLINE_JUDGE
+ 
+    freopen("input.txt", "r", stdin);
+ 
+    freopen("output.txt", "w", stdout);
+   
+#endif
+    
+    
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     

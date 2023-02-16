dd29c300c7b970d23accf122cd7c5907767a892e
iamattri0001 <105377205+iamattri0001@users.noreply.github.com>
2023-02-16 19:56:21 +0530

    Create subl_boiler_plate.cpp

---
+++ subl_boiler_plate.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define vi vector<int>
+#define vll vector<long long int>
+#define pb push_back
+#define loop(n) for (int i = 0; i < n; i++)
+#define loopRange(a, b) for (int i = a; i < b; i++)
+#define INF 1e9
+#define MIN_INF -1e9
+#define sortAll(v) sort(v.begin(), v.end())
+#define newL cout << "\n"
+#define vec_in(v, n)            \
+    for (int i = 0; i < n; i++) \
+    cin >> v[i]
+
+#define vec_out(v, n)           \
+    for (int i = 0; i < n; i++) \
+    cout << v[i] << " "
+
+#define umap unordered_map
+#define uset unordered_set
+
+
+int32_t main()
+{
+     
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int t;
+    cin>>t;
+    while(t--){
+        cout << t;
+
+        newL;
+    }
+
+    return 0;
+}

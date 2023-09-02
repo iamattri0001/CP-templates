a970717447f7ca3e8336b923cf1fdbe34e57b97f
iamattri0001 <105377205+iamattri0001@users.noreply.github.com>
2023-09-02 23:37:18 +0530

    Update BIT.cpp

--- BIT.cpp
+++ BIT.cpp
@@ -1,3 +1,7 @@
+// 1. BIT t(size of the array);
+// 2. t.build(the input array as vector)
+// 3. use 1-based indexing for t.sum(from index, to index) and t.add(index, value to be added);
+
 class BIT{
     vector<long long> tree;
     int N;
@@ -5,6 +9,7 @@ class BIT{
     long long sum(int i){
         long long ans = 0;
         for(; i>0; i -= (i & (-i)))
+            // i & (-i) gives highest set-bit in 'i'
             ans += tree[i];
         return ans;
     }
@@ -15,12 +20,17 @@ public:
         tree.resize(n+1);
     }
 
-    long long query(int l, int r){
+    long long sum(int l, int r){
         return sum(r) - sum(l-1);
     }
 
-    void update(int i, long long x){
+    void add(int i, long long x){
         for(; i <= N; i += (i & (-i)))
             tree[i] += x;
+    }
+
+    void build(vector<int> &v) {
+        for(int i=0; i<N; i++)
+            add(i+1, v[i]);
     }
 };

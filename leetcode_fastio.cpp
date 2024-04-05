c9e20073eee674ea0889d8b60700f1a2870fd90f
iamattri0001 <105377205+iamattri0001@users.noreply.github.com>
2024-04-05 14:28:58 +0530

    Update leetcode_fastio.cpp

--- leetcode_fastio.cpp
+++ leetcode_fastio.cpp
@@ -1,5 +1,8 @@
-int speed_up = []{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);cout.tie(NULL);
-    return 0;
+#pragma GCC optimize("O3","unroll-loops")
+
+static const bool Booster = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return true;
 }();

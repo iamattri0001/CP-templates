4492fc487843146d68c4bb3dd71923b9d675aa53
iamattri0001 <105377205+iamattri0001@users.noreply.github.com>
2024-10-11 17:41:23 +0530

    Update SparseRMQ.cpp

--- SparseRMQ.cpp
+++ SparseRMQ.cpp
@@ -1,15 +1,14 @@
-// Range min/max queries in O(1) with O(nlogn) pre-processing
-
 class SparseRMQ {
     vector<vector<int>> st;
     int K;
+    vector<int> lg;
 
-    int log2_floor(unsigned long long i) {
-        return i ? __builtin_clzll(1) - __builtin_clzll(i) : -1;
+    static int log2_floor(unsigned long long i) { // not in use
+        return i ?  : -1;
     }
 
     inline int f(int x, int y) {
-        return min(x, y); // change here for max/min
+        return max(x, y); // change here for max/min
     }
 
 public:
@@ -17,17 +16,28 @@ public:
         int n = nums.size();
         K = log2(n);
 
-        st.resize(K + 1, vector<int>(n));
-        for(int i = 0; i < n; i++)
-            st[0][i] = nums[i];
+        // pre-compute all log2 for O(1) query
+        lg.resize(n + 1);
+        lg[0] = -1;
+        for(int i = 1; i <= n; i++) {
+            lg[i] = __builtin_clzll(1) - __builtin_clzll(i);
+        }
 
-        for(int i = 1; i <= K; i++)
-            for(int j = 0; j + (1 << i) <= n; j++)
+        
+        st.resize(K + 1, vector<int>(n));
+        for(int i = 0; i < n; i++) {
+            st[0][i] = nums[i];
+        }
+
+        for(int i = 1; i <= K; i++) {
+            for(int j = 0; j + (1 << i) <= n; j++) {
                 st[i][j] = f(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
+            }
+        }
     }
 
-    int RMQ(int a, int b) { // zero based indexing
-        int i = log2_floor(b - a + 1);
+    int RMQ(int a, int b) { // 0 based indexing
+        int i = lg[b - a + 1];
         return f(st[i][a], st[i][b - (1 << i) + 1]);
     }
 };
